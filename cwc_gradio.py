625aa0faf87c3cba6e20ec039df1ea289493bf3d
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-08-07 14:15:11 -0400

    minor gradio tweaks

--- cwc_gradio.py
+++ cwc_gradio.py
@@ -27,18 +27,18 @@ class CWCGradio:
                         "documents and user queries.")
             with gr.Row():
                 with gr.Column(scale=8):
-                    chatbot = gr.Chatbot(label="RAG Chat", height=600, container= False, show_copy_button=True)
+                    chatbot = gr.Chatbot(label="RAG Chat", height=600, container=False, show_copy_button=True)
                     msg = gr.Textbox(show_label=False, autoscroll=True, autofocus=True, container=False,
                                      placeholder="Enter your coding question here...")
                     with gr.Row():
                         clear = gr.ClearButton([msg, chatbot])
                     msg.submit(self.chat, inputs=[msg], outputs=[msg, chatbot], show_progress="full")
                 with gr.Column(scale=1):
-                    selected_model = gr.Dropdown(container= False,
+                    selected_model = gr.Dropdown(
                         choices=["codestral:latest", "mistral-nemo:latest", "llama3.1:latest",
                                  "deepseek-coder-v2:latest", "gemma2:latest", "codegemma:latest"],
                         label="Select Model", value="codestral:latest", interactive=True)
-                    gr.Files(label="Upload Your Files", container=False)
+                    gr.Files(label="Upload Your Files")
                 selected_model.change(self.update_model, inputs=selected_model, show_progress="full")
 
         iface.launch(inbrowser=True, share=True)
