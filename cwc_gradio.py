3d7ec2f0729b7ecb51b1cd07e17c816ea4b8f712
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-08-10 08:25:14 -0400

    minor gradio tweaks

--- cwc_gradio.py
+++ cwc_gradio.py
@@ -22,24 +22,24 @@ class CWCGradio:
             gr.Markdown("# Chat RAG: Interactive Coding Assistant")
             gr.Markdown("### This app is a chat-based coding assistant with a graphical user interface built using "
                         "Gradio. It allows users to interact with various language models to ask coding questions, "
-                        "with the ability to upload files for additional context. \n\n### The app utilizes RAG ("
+                        "with the ability to upload files for additional context. \n### The app utilizes RAG ("
                         "Retrieval-Augmented Generation) to provide more informed responses based on the loaded "
                         "documents and user queries.")
             with gr.Row():
-                with gr.Column(scale=8):
+                with gr.Column(scale=8, variant="compact"):
                     chatbot = gr.Chatbot(label="RAG Chat", height=600, container=False, show_copy_button=True)
                     msg = gr.Textbox(show_label=False, autoscroll=True, autofocus=True, container=False,
                                      placeholder="Enter your coding question here...")
                     with gr.Row():
-                        clear = gr.ClearButton([msg, chatbot])
+                        gr.ClearButton([msg, chatbot])
                     msg.submit(self.chat, inputs=[msg], outputs=[msg, chatbot], show_progress="full")
                 with gr.Column(scale=1):
                     gr.Files(label="Upload Your Files")
-                    selected_model = gr.Dropdown(
+                    selected_chat_model = gr.Dropdown(
                         choices=["codestral:latest", "mistral-nemo:latest", "llama3.1:latest",
                                  "deepseek-coder-v2:latest", "gemma2:latest", "codegemma:latest"],
-                        label="Select Model", value="codestral:latest", interactive=True)
-
-                selected_model.change(self.update_model, inputs=selected_model, show_progress="full")
+                        label="Select Chat Model", value="codestral:latest", interactive=True)
+                selected_chat_model.change(self.update_model, inputs=selected_chat_model)
 
         iface.launch(inbrowser=True, share=True)
+
