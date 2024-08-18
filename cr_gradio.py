1b1df8e80c88e578fe5f2cece0cf2baaf6ae6045
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-08-18 19:58:05 -0400

    updated and removed some functions and return statements that were unnecessary. Changed program look

--- cr_gradio.py
+++ cr_gradio.py
@@ -2,16 +2,12 @@ import os
 import shutil
 import gradio as gr
 from model_utils import ModelManager
-from dotenv import load_dotenv
-
-# load_dotenv()
 
 
 class CWCGradio:
     def __init__(self):
         self.model_temp = .75
         self.max_tokens = 2048
-        self.file_paths = None
         self.model_manager = ModelManager()
         self.chat_history = []
         self.model_display_names = {
@@ -20,13 +16,13 @@ class CWCGradio:
             "Gemma2 9B": "gemma2:latest", "CodeGemma 7B": "codegemma:latest"
         }
 
-    def chat(self, message):
-        response = self.model_manager.process_input(message)
+    async def chat(self, message: str):
+        response = await self.model_manager.process_input(message)
         self.chat_history.append((message, str(response)))
         return "", self.chat_history
 
-    def stream_response(self, message):
-        streaming_response = self.model_manager.process_input(message)
+    async def stream_response(self, message: str):
+        streaming_response = await self.model_manager.process_input(message)
         full_response = ""
         for delta in streaming_response.response_gen:
             full_response += delta
@@ -35,13 +31,9 @@ class CWCGradio:
 
     def clear_chat_history(self):
         self.chat_history.clear()
-        return self.chat_history
 
     def clear_his_and_mem(self):
         self.clear_chat_history()
-        self.model_manager.reset_chat_engine()
-
-    def upload_button(self):
         self.model_manager.reset_chat_engine()
 
     def delete_db(self):
@@ -53,16 +45,12 @@ class CWCGradio:
         self.model_manager.reset_chat_engine()
 
     def update_max_tokens(self, max_tokens):
-        self.max_tokens = max_tokens
         self.model_manager.update_max_tokens(max_tokens)
         gr.Warning("WARNING: This may cut the output of the model short if your response requires more tokens "
                    "for the answer!!!", duration=10)
-        return max_tokens
 
     def update_model_temp(self, temperature):
-        self.model_temp = temperature
         self.model_manager.update_temperature(temperature)
-        return self.model_temp
 
     def update_model(self, display_name):
         model_name = self.model_display_names.get(display_name, "codestral:latest")
@@ -70,12 +58,11 @@ class CWCGradio:
         self.chat_history.clear()
         gr.Warning(f"Model updated to {display_name}. Please make sure you have this model installed through "
                    "Ollama!", duration=10)
-        return []
 
-    def handle_doc_upload(self, files):
+    @staticmethod
+    async def handle_doc_upload(files):
         gr.Warning("Make sure you hit the upload button or the model wont see your files!", duration=10)
-        self.file_paths = [file.name for file in files]
-        return self.file_paths
+        return [file.name for file in files]
 
     def launch(self):
         with gr.Blocks(title="Chat RAG", theme="monochrome", fill_height=True, fill_width=True) as iface:
@@ -96,10 +83,9 @@ class CWCGradio:
                     msg.submit(self.stream_response, inputs=[msg], outputs=[msg, chatbot], show_progress="full")
 
                 with gr.Column(scale=2):
-                    files = gr.Files(interactive=True, label="Upload Files Here", container=False,
-                                     file_count="multiple", file_types=["text", ".pdf", ".py", ".txt", ".dart", ".c",
-                                                                        ".css", ".cpp", ".html", ".docx", ".doc", ".js",
-                                                                        ".jsx", ".xml"])
+                    files = gr.Files(interactive=True, label="Upload Files Here", file_count="multiple",
+                                     file_types=["text", ".pdf", ".py", ".txt", ".dart", ".c", ".jsx", ".xml",
+                                                 ".css", ".cpp", ".html", ".docx", ".doc", ".js"])
                     with gr.Row():
                         upload = gr.Button(value="Upload Data", interactive=True)
                         clear_db = gr.Button(value="Clear RAG Database", interactive=True)
@@ -109,7 +95,6 @@ class CWCGradio:
                     max_tokens = gr.Slider(minimum=100, maximum=5000, value=2048, step=1,
                                            label="Max Tokens in Response",
                                            info="Set the maximum number of tokens the model can respond with.")
-                    temp_state = gr.State(value=.75)
                     selected_chat_model = gr.Dropdown(choices=list(self.model_display_names.keys()), interactive=True,
                                                       label="Select Chat Model", value="Codestral 22B", filterable=True,
                                                       info="Choose the model you want to chat with from the list below."
@@ -120,10 +105,10 @@ class CWCGradio:
                 clear_chat_mem.click(self.clear_his_and_mem, outputs=chatbot)
                 # Buttons in Right Column
                 files.upload(self.handle_doc_upload, show_progress="full")
-                upload.click(self.upload_button)
+                upload.click(lambda: self.model_manager.reset_chat_engine())
                 clear_db.click(self.delete_db, show_progress="full")
-                temperature.release(self.update_model_temp, inputs=[temperature], outputs=[temp_state])
-                max_tokens.release(self.update_max_tokens, inputs=[max_tokens], outputs=[max_tokens])
+                temperature.release(self.update_model_temp, inputs=[temperature])
+                max_tokens.release(self.update_max_tokens, inputs=[max_tokens])
                 selected_chat_model.change(self.update_model, inputs=selected_chat_model, outputs=[chatbot])
 
         iface.launch(inbrowser=True, share=True)
