6d814b3e19a2060ad9d10bf40ebc94f716c30487
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-08-29 07:57:36 -0400

    updated prompt

--- utils.py
+++ utils.py
@@ -95,14 +95,14 @@ def setup_index_and_chat_engine(docs, embed_model, llm, memory, custom_prompt):
     index = VectorStoreIndex.from_documents(docs, embed_model=embed_model)
     Settings.llm = llm
     chat_prompt = (
-        "You are an AI coding assistant, your primary function is to help users with coding-related questions\n"
-        "and tasks. You have access to a knowledge base of programming documentation and best practices.\n"
-        "When answering questions please follow these guidelines. 1. Provide clear, concise, and\n"
-        "accurate code snippets when appropriate. 2. Explain your code and reasoning step by step. 3. Offer\n"
-        "suggestions for best practices and potential optimizations. 4. If the user's question is unclear,\n"
+        "You are an AI coding assistant, your primary function is to help users with coding-related questions \n"
+        "and tasks. You have access to a knowledge base of programming documentation and best practices. \n"
+        "When answering questions please follow these guidelines. 1. Provide clear, concise, and \n"
+        "accurate code snippets when appropriate. 2. Explain your code and reasoning step by step. 3. Offer \n"
+        "suggestions for best practices and potential optimizations. 4. If the user's question is unclear, \n"
         "ask for clarification dont assume or guess the answer to any question. 5. When referencing external \n"
-        "libraries or frameworks, briefly explain their purpose. 6. If the question involves multiple possible\n"
-        "approaches, outline the pros and cons of each. Always Remember to be friendly!\n"
+        "libraries or frameworks, briefly explain their purpose. 6. If the question involves multiple possible \n"
+        "approaches, outline the pros and cons of each. Always Remember to be friendly! \n"
         "Response:"
     )
 
@@ -118,8 +118,8 @@ def setup_index_and_chat_engine(docs, embed_model, llm, memory, custom_prompt):
                         "{context_str}\n"
                         "---------------------\n"
                         "Given the context information above I want you to think step by step to answer \n"
-                        "the query in a crisp manner, incase case you don't know the answer say 'I don't know!'.\n"
-                        "Query: {query_str}\n"
+                        "the query in a crisp manner, incase case you don't know the answer say 'I don't know!'. \n"
+                        "Query: {query_str} \n"
                         "Answer: ")
     )
     return chat_engine
